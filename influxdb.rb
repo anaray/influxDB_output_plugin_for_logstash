4d37a0faf1a4e05e3a91a0faad6abf79d946a736
Anumodh Narayanan <anumodh@gmail.com>
2014-05-15 12:26:25 +0530

    influx db output plugin

---
+++ influxdb.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+require "logstash/namespace"
+require "logstash/outputs/base"
+
+class LogStash::Outputs::InfluxDB < LogStash::Outputs::Base
+  config_name "influxdb"
+  milestone 1
+
+  # username to connect to influxdb
+  config :username, :validate => :string, :required => :true
+  config :password, :validate => :string, :required => :true
+  config :database, :validate => :string, :required => :true
+  config :series_name, :validate => :string, :required => :true
+
+
+  public
+  def register
+    require 'influxdb'
+    @influxdb = InfluxDB::Client.new @database, :username => @username, :password => @password
+  end
+
+  public
+  def receive(event)
+    return unless output?(event)
+    puts "event from influxdb .."
+    #puts event.to_hash
+    puts "Username > #{@username}"
+  end
+end
